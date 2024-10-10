9f9fa79178b39f70eb8fe00cc7a0033051534574
Sebastian Göb <sebastiancgoeb@gmail.com>
2024-10-10 21:19:41 +0200

    feat: Einstellungsseite für Schwimmer

--- web/src/pages/Schwimmer/Schwimmer.tsx
+++ web/src/pages/Schwimmer/Schwimmer.tsx
@@ -1,11 +1,75 @@
 import "./Schwimmer.module.css";
-import { Container } from "@mantine/core";
+import { Button, Checkbox, Container, Group, NativeSelect, NumberInput, Paper, Table } from "@mantine/core";
+import React from "react";
+import { useStore } from "../../services/state.ts";
+import { useShallow } from "zustand/react/shallow";
+import { Swimmer } from "../../model/swimmer.ts";
+import { Gender } from "../../model/gender.ts";
+import { IconPresentation } from "@tabler/icons-react";
+import { demoData1 } from "../../demo/data.ts";
+
+function numberify(sn: string | number): number | undefined {
+  if (typeof sn === "string") {
+    return undefined;
+  }
+  return sn;
+}
 
 export default function Schwimmer() {
+  const [swimmers, updateEverything, updateSwimmer] = useStore(
+    useShallow((state) => [state.swimmers, state.updateEverything, state.updateSwimmer]),
+  );
+
+  function renderRow(swimmer: Swimmer): React.ReactNode[] {
+    //   TODO min/max dynamic
+    return [
+      swimmer.name,
+      <Checkbox
+        color="gray"
+        checked={swimmer.present}
+        onChange={(evt) => updateSwimmer({ ...swimmer, present: evt.currentTarget.checked })}
+      />,
+      <NativeSelect
+        data={[Gender.M, Gender.W]}
+        value={swimmer.gender}
+        onChange={(evt) => updateSwimmer({ ...swimmer, gender: evt.currentTarget.value as Gender })}
+      />,
+      <NumberInput
+        min={0}
+        max={5}
+        placeholder="0-5"
+        clampBehavior="strict"
+        value={swimmer.minStarts}
+        onChange={(value) => updateSwimmer({ ...swimmer, minStarts: numberify(value) })}
+      />,
+      <NumberInput
+        min={0}
+        max={5}
+        placeholder="0-5"
+        clampBehavior="strict"
+        value={swimmer.maxStarts}
+        onChange={(value) => updateSwimmer({ ...swimmer, maxStarts: numberify(value) })}
+      />,
+    ];
+  }
+
   return (
     <Container size="md">
-      <h1>Schwimmer</h1>
-      <p>Hier kommt bald was.</p>
+      <Group justify="space-between">
+        <h1>Schwimmer</h1>
+        <Button rightSection={<IconPresentation />} onClick={() => updateEverything(demoData1)}>
+          Demodaten nutzen
+        </Button>
+      </Group>
+
+      <Paper shadow="md" withBorder p="xl">
+        <Table
+          data={{
+            head: ["Name", "Anwesend", "Geschlecht", "Min Starts", "Max Starts"],
+            body: Array.from(swimmers.values()).map(renderRow),
+          }}
+        ></Table>
+      </Paper>
     </Container>
   );
 }
