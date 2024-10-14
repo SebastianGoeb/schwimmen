74437fadd15d25e84f2b07d79ec07e45eccfdcc9
Sebastian Göb <sebastiancgoeb@gmail.com>
2024-10-14 14:40:59 +0200

    also works in discipline view

--- web/src/components/LapTimeDisciplinesView/LapTimeDisciplinesView.tsx
+++ web/src/components/LapTimeDisciplinesView/LapTimeDisciplinesView.tsx
@@ -3,7 +3,7 @@ import { useStore } from "../../services/state.ts";
 import { useShallow } from "zustand/react/shallow";
 import { Discipline } from "../../model/discipline.ts";
 import React from "react";
-import { Swimmer } from "../../model/swimmer.ts";
+import { compareByYearThenGenderThenLastname, Swimmer } from "../../model/swimmer.ts";
 import LapTimeCell from "../LapTimeCell/LapTimeCell.tsx";
 import SwimmerAddButton from "../SwimmerAddButton/SwimmerAddButton.tsx";
 import SwimmerRemoveButton from "../SwimmerRemoveButton/SwimmerRemoveButton.tsx";
@@ -12,16 +12,21 @@ import SwimmerNameInput from "../SwimmerNameInput/SwimmerNameInput.tsx";
 export default function LapTimeDisciplinesView() {
   const [disciplines, swimmers] = useStore(useShallow((state) => [state.disciplines, state.swimmers]));
 
+  const swimmersSorted = Array.from(swimmers.values()).sort(compareByYearThenGenderThenLastname);
+
   function renderDiscipline(discipline: Discipline): React.ReactNode {
     return (
       <Paper shadow="md" withBorder p="xl" key={discipline.id}>
         <h2>{discipline.name}</h2>
-        <Table
-          data={{
-            head: ["Name", "Zeit"],
-            body: Array.from(swimmers.values()).map((swimmer) => renderRow(swimmer, discipline)),
-          }}
-        />
+        <Table>
+          <Table.Thead>
+            <Table.Tr>
+              <Table.Th> Name</Table.Th>
+              <Table.Th> Zeit</Table.Th>
+            </Table.Tr>
+          </Table.Thead>
+          <Table.Tbody>{swimmersSorted.map((swimmer) => renderRow(swimmer, discipline))}</Table.Tbody>
+        </Table>
         <Space h="md" />
         <Group justify="flex-end">
           <SwimmerAddButton />
@@ -30,12 +35,20 @@ export default function LapTimeDisciplinesView() {
     );
   }
 
-  function renderRow(swimmer: Swimmer, discipline: Discipline): React.ReactNode[] {
-    return [
-      <SwimmerNameInput swimmer={swimmer} />,
-      <LapTimeCell swimmer={swimmer} disciplineId={discipline.id} />,
-      <SwimmerRemoveButton id={swimmer.id} />,
-    ];
+  function renderRow(swimmer: Swimmer, discipline: Discipline): React.ReactNode {
+    return (
+      <Table.Tr key={swimmer.id}>
+        <Table.Td>
+          <SwimmerNameInput swimmer={swimmer} />
+        </Table.Td>
+        <Table.Td>
+          <LapTimeCell swimmer={swimmer} disciplineId={discipline.id} />
+        </Table.Td>
+        <Table.Td>
+          <SwimmerRemoveButton id={swimmer.id} />
+        </Table.Td>
+      </Table.Tr>
+    );
   }
 
   return (
