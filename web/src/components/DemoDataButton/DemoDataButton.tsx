df9cad34242dc2a0d58e1af856278e7c6f1a8c66
Sebastian Göb <sebastiancgoeb@gmail.com>
2024-10-15 15:03:09 +0200

    separate dev page

--- web/src/components/DemoDataButton/DemoDataButton.tsx
+++ web/src/components/DemoDataButton/DemoDataButton.tsx
@@ -1,4 +1,4 @@
-import { IconPresentation } from "@tabler/icons-react";
+import { IconRestore } from "@tabler/icons-react";
 import { demoData1 } from "../../demo/data.ts";
 import { Button, Text } from "@mantine/core";
 import { useStore } from "../../services/state.ts";
@@ -7,9 +7,8 @@ export default function DemoDataButton() {
   const updateEverything = useStore((state) => state.updateEverything);
 
   return (
-    <Button rightSection={<IconPresentation />} onClick={() => updateEverything(demoData1)}>
-      <Text visibleFrom="sm">Auf Demodaten zurücksetzen</Text>
-      <Text hiddenFrom="sm">Demodaten</Text>
+    <Button variant="outline" rightSection={<IconRestore />} onClick={() => updateEverything(demoData1)}>
+      Auf Demodaten zurücksetzen
     </Button>
   );
 }
