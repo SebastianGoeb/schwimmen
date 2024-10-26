6b072e5bb093fbf16e82caa15ef9cdfc3862b9d8
Sebastian Göb <sebastiancgoeb@gmail.com>
2024-10-26 22:27:45 +0200

    feat: Daten werden im Browser gespeichert

--- web/src/services/state/state.ts
+++ web/src/services/state/state.ts
@@ -4,13 +4,58 @@ import { createSwimmerSlice, SwimmerSlice } from "./swimmer.ts";
 import { createRelaySlice, RelaySlice } from "./relay.ts";
 import { createSharedSlice, SharedSlice } from "./shared.ts";
 import { createSettingsSlice, SettingsSlice } from "./settings.ts";
+import { persist, StorageValue } from "zustand/middleware";
+import { Swimmer } from "../../model/swimmer.ts";
 
 export type CombinedState = SharedSlice & DisciplineSlice & SwimmerSlice & RelaySlice & SettingsSlice;
 
-export const useCombinedStore = create<CombinedState>()((...a) => ({
-  ...createSharedSlice(...a),
-  ...createDisciplineSlice(...a),
-  ...createSwimmerSlice(...a),
-  ...createRelaySlice(...a),
-  ...createSettingsSlice(...a),
-}));
+export const useCombinedStore = create<CombinedState>()(
+  persist(
+    (...a) => ({
+      ...createSharedSlice(...a),
+      ...createDisciplineSlice(...a),
+      ...createSwimmerSlice(...a),
+      ...createRelaySlice(...a),
+      ...createSettingsSlice(...a),
+    }),
+    {
+      name: "combined-store",
+      storage: {
+        getItem: (name): StorageValue<CombinedState> | null => {
+          const str = localStorage.getItem(name);
+          if (!str) return null;
+          const existingValue = JSON.parse(str);
+          return {
+            ...existingValue,
+            state: {
+              ...existingValue.state,
+              swimmers: new Map(
+                (existingValue.state.swimmers as [number, Swimmer][]).map(([id, swimmer]) => [
+                  id,
+                  { ...swimmer, lapTimes: new Map(swimmer.lapTimes) },
+                ]),
+              ),
+              relays: new Map(existingValue.state.relays),
+            },
+          };
+        },
+        setItem: (name, newValue: StorageValue<CombinedState>) => {
+          // functions cannot be JSON encoded
+          const str = JSON.stringify({
+            ...newValue,
+            state: {
+              ...newValue.state,
+              swimmers: Array.from(newValue.state.swimmers.entries(), ([id, swimmer]) => [
+                id,
+                { ...swimmer, lapTimes: Array.from(swimmer.lapTimes.entries()) },
+              ]),
+              relays: Array.from(newValue.state.relays.entries()),
+            },
+          });
+          localStorage.setItem(name, str);
+        },
+        removeItem: (name) => localStorage.removeItem(name),
+      },
+    },
+  ),
+);
