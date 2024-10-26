6cab87b24d3c4a66a66e86b2f801b28c38aeef2f
Sebastian Goeb <SebastianGoeb@users.noreply.github.com>
2024-10-26 22:29:43 +0200

    feat: Daten werden im Browser gespeichert (#56)

--- web/src/components/LapTimeCell/LapTimeCell.tsx
+++ web/src/components/LapTimeCell/LapTimeCell.tsx
@@ -4,6 +4,7 @@ import { useShallow } from "zustand/react/shallow";
 import { Swimmer } from "../../model/swimmer.ts";
 import { IMaskInput } from "react-imask";
 import { zeitenMask } from "../../utils/input-mask.ts";
+import { isEqual } from "lodash-es";
 
 export default function LapTimeCell({ swimmer, disciplineId }: { swimmer: Swimmer; disciplineId: number }) {
   const [updateLapTime, removeLapTime] = useCombinedStore(
@@ -23,16 +24,26 @@ export default function LapTimeCell({ swimmer, disciplineId }: { swimmer: Swimme
           if (value === "" || value === "__:__,__") {
             removeLapTime(swimmer, disciplineId);
           } else {
-            updateLapTime(swimmer, disciplineId, { enabled: lapTime?.enabled ?? true, seconds: value });
+            // remove 100s of unnecessary local storage writes on page load
+            if (!isEqual(lapTime?.seconds, value)) {
+              updateLapTime(swimmer, disciplineId, { enabled: lapTime?.enabled ?? true, seconds: value });
+            }
           }
         }}
       ></Input>
       <Checkbox
         color="dimmed"
         disabled={lapTime === undefined}
-        onChange={(evt) =>
-          updateLapTime(swimmer, disciplineId, { seconds: lapTime?.seconds ?? "", enabled: evt.currentTarget.checked })
-        }
+        onChange={(evt) => {
+          const value = evt.currentTarget.checked;
+          if (!isEqual(lapTime?.enabled, value)) {
+            // remove 100s of unnecessary local storage writes on page load
+            updateLapTime(swimmer, disciplineId, {
+              seconds: lapTime?.seconds ?? "",
+              enabled: value,
+            });
+          }
+        }}
         checked={lapTime?.enabled ?? true}
       />
     </Group>
