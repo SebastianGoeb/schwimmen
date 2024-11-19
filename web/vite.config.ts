3c1a82a8c7a32ccec5def98314193e6fb73532da
Sebastian Göb <sebastiancgoeb@gmail.com>
2024-11-19 14:39:18 +0100

    improve version text

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -2,17 +2,14 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import * as child from "child_process";
 
-const commitHash = child.execSync("git rev-parse --short HEAD").toString().trim();
-const worktreeString = child.execSync("git status --porcelain").toString().trim();
-const worktreeClean = worktreeString.length === 0;
-
-console.log(worktreeString, worktreeClean);
+const describe = child.execSync("git describe --tags --always").toString().trim();
+const dirty = child.execSync("git status --porcelain").toString().trim().length > 0;
 
 // https://vitejs.dev/config/
 export default defineConfig({
   // make sure to also declare these in vite-env.d.ts
   define: {
-    __APP_VERSION__: JSON.stringify(commitHash + (worktreeClean ? "" : "-dirty")),
+    __APP_VERSION__: JSON.stringify(describe + (dirty ? "-dirty" : "")),
   },
   plugins: [react()],
 });
